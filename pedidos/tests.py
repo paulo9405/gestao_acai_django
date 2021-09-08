f8f917aaf1a38cc0016194ac94d11cdfada6a699
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-09-08 14:10:57 -0300

    #ACAI-36 Criando testes para classe cadastro de acrescimos.

--- pedidos/tests.py
+++ pedidos/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
-from .models import CadastroAcai
-from .forms import CadastroAcaiForm
+from .models import CadastroAcai, CadastroAcrescimos
+from .forms import CadastroAcaiForm, CadastroAcrecimosForm
 
 
 class CadastroAcaiTestCase(TestCase):
@@ -61,3 +61,57 @@ class CadastroAcaiTestCase(TestCase):
         )
 
         self.assertIsNotNone(acai)
+
+class CadastroAcrescimosTestCase(TestCase):
+
+    def test_form_cad_acre_is_valid(self):
+        """
+        Teste para validar o fomulario de cadastro de acrescimos é valido
+        """
+        data_model = {
+            "nome": 'Chocolate',
+            "valor": 2.00,
+        }
+
+        form = CadastroAcrecimosForm(data=data_model)
+        self.assertTrue(form.is_valid())
+
+    def test_form_cad_acre_is_not_valid(self):
+        """
+        Teste para dar erro de formulario, prenchimento errado
+        """
+        data_model = {
+            "nome": 123,
+            "valor": 'letras',
+        }
+
+        form = CadastroAcrecimosForm(data=data_model)
+        self.assertFalse(form.is_valid())
+
+    def test_form_cad_acre_field_empty(self):
+        """
+        Teste para dar erro de formulario com campo obrigatório vazio.
+        """
+        data_model = {
+            "nome": '',
+            "valor": '',
+        }
+
+        form = CadastroAcrecimosForm(data=data_model)
+        self.assertFalse(form.is_valid())
+
+    def test_cadastrar_acrescimo(self):
+        """
+        Teste para criar acrescimo e verificando se os dados não são None.
+        """
+        data = {
+            "nome": 'Chocolate',
+            "valor": 2,
+        }
+
+        acrescimo = CadastroAcrescimos.objects.create(
+            nome=data['nome'],
+            valor=data['valor'],
+        )
+
+        self.assertIsNotNone(acrescimo)
