6901e88f8ee8e6f0df24121db24589f2b95adabe
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-09-08 12:22:12 -0300

    #ACAI-33 feito app de pedidos e class de criar açai.

--- gestao_acai/settings.py
+++ gestao_acai/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'bootstrapform',
     'core',
+    'pedidos',
 ]
 
 MIDDLEWARE = [
---
+++ pedidos/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class CadastroAcai(models.Model):
+    nome = models.CharField(max_length=50)
+    tamanho = models.CharField(max_length=20)
+    valor = models.DecimalField(max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return str(self.nome) + ' / ' + str(self.tamanho) + ' / R$ ' + str(self.valor)
