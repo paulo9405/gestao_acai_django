08b16dc6c4dd30a3178fd41a3024020e9a5291e6
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-09-09 16:20:44 -0300

    #ACAI-33 criando class pedidos.

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -1,5 +1,19 @@
 from django.contrib import admin
-from .models import CadastroAcai, CadastroAcrescimos
+from .models import CadastroAcai, CadastroAcrescimos, Pedido
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = (
+        'dia',
+        'nome_cliente',
+        'telefone',
+        'endereco',
+        'pagamento',
+        'acai_pedido',
+        'acrescimo_pedido',
+        'valor_pagar',
+    )
+
 
 admin.site.register(CadastroAcai)
 admin.site.register(CadastroAcrescimos)
+admin.site.register(Pedido, PedidoAdmin)
--- pedidos/models.py
+++ pedidos/models.py
@@ -16,3 +16,19 @@ class CadastroAcrescimos(models.Model):
 
     def __str__(self):
         return str(self.nome) + '/  R$' + str(self.valor)
+
+
+class Pedido(models.Model):
+    PAG = (('Dinheiro', 'Dinheiro'), ('Cartão', 'Cartão'),)
+    dia = models.DateTimeField(auto_now=True)
+    nome_cliente = models.CharField(max_length=100)
+    telefone = models.CharField(max_length=20, blank=True)
+    endereco = models.CharField(max_length=200)
+    pagamento = models.CharField(max_length= 20, choices=PAG)
+    acai_pedido = models.ForeignKey(CadastroAcai, on_delete=models.CASCADE)
+    acrescimo_pedido = models.ForeignKey(CadastroAcrescimos, on_delete=models.CASCADE)
+
+    valor_pagar = models.DecimalField(blank=True, default=0, max_digits=7, decimal_places=2)
+
+    def __str__(self):
+        return str(self.acai_pedido) + ' - ' + str(self.nome_cliente)
