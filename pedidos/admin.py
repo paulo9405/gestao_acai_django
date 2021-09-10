1e0d5e4ee52751710a08242d5ba5d0c757481da1
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-09-10 12:06:04 -0300

    #ACAI-33 adicionando filtros para pedidos no admin e um atributo observações.

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -3,15 +3,32 @@ from .models import CadastroAcai, CadastroAcrescimos, Pedido
 
 class PedidoAdmin(admin.ModelAdmin):
     list_display = (
+        'id',
         'dia',
         'nome_cliente',
         'telefone',
         'endereco',
-        'pagamento',
         'acai_pedido',
         'acrescimo_pedido',
+        'pagamento',
         'valor_pagar',
     )
+
+    # def total(self, obj):
+    #     return obj.get_total()
+    #
+    # total.short_description = 'Total'
+
+    fieldsets = (
+        ('Dados do Cliente', {'fields': ('nome_cliente', 'telefone', 'endereco')}),
+        ('Dados do Pedido', {'fields': ('acai_pedido', 'acrescimo_pedido', 'pagamento', 'observacoes', 'valor_pagar')}),
+    )
+
+    readonly_fields = ('valor_pagar',)
+    search_fields = ('id', 'nome_cliente')
+
+
+
 
 
 admin.site.register(CadastroAcai)
--- pedidos/models.py
+++ pedidos/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models import Sum, F
+from django.forms import FloatField
 
 
 class CadastroAcai(models.Model):
@@ -24,11 +26,44 @@ class Pedido(models.Model):
     nome_cliente = models.CharField(max_length=100)
     telefone = models.CharField(max_length=20, blank=True)
     endereco = models.CharField(max_length=200)
-    pagamento = models.CharField(max_length= 20, choices=PAG)
     acai_pedido = models.ForeignKey(CadastroAcai, on_delete=models.CASCADE)
     acrescimo_pedido = models.ForeignKey(CadastroAcrescimos, on_delete=models.CASCADE)
+    pagamento = models.CharField(max_length=20, choices=PAG)
+    observacoes = models.TextField(blank=True)
 
     valor_pagar = models.DecimalField(blank=True, default=0, max_digits=7, decimal_places=2)
 
+
+    #TODO: criar funçõa de soma pedido mais acrescimo
+
+    # def get_total(self):
+    #     tot_acai = 0
+    #     for acai in self.acai_pedido.all():
+    #         tot_acai += acai.valor
+    #
+    #     return (tot_acai)
+
+        # tot_acai = 0
+        # tot_acre = 0
+        # for acai in self.acai_pedido__:
+        #     tot_acai += acai.valor
+        #     for acre in self.acrescimo_pedido.all():
+        #         tot_acre += acre.valor
+        # return (tot_acre + tot_acai)
+
+
+
     def __str__(self):
         return str(self.acai_pedido) + ' - ' + str(self.nome_cliente)
+
+    # def soma_venda_dinheiro_cartao(self):
+    #     self.valor_pagar = Pedido.objects.aggregate(
+    #         tot_d_c=Sum((F('acai_pedido__valor') + F('acrescimo_pedido__valor')),
+    #                     output_field=FloatField()))['tot_d_c'] or 0
+    #     self.save()
+    #     return self.valor_pagar
+
+    # def save(self, *args, **kwargs):
+    #     self.valor_pagar = (self.acai_pedido__valor + self.acrescimo_pedido__valor)
+    #     return super(Pedido, self).save(*args, **kwargs)
+
