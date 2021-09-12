63bf3d3d3b42f111dac823934d9b88e0ba94e30e
Paulo Ricardo <paulo.ricardo1137.pr@gmail.com>
2021-09-11 23:27:10 -0300

    #ACAI-33 adicionando auto complete field para acrescimos e açais.

--- pedidos/admin.py
+++ pedidos/admin.py
@@ -4,10 +4,12 @@ from .models import CadastroAcai, CadastroAcrescimos, Pedido
 
 class CadastroAcaiAdmin(admin.ModelAdmin):
     list_display = ('nome', 'tamanho', 'valor', 'id')
+    search_fields = ('id', 'nome')
 
 
 class CadastroAcrecimosAdmin(admin.ModelAdmin):
     list_display = ('nome', 'valor', 'id')
+    search_fields = ('id', 'nome')
 
 
 class PedidoAdmin(admin.ModelAdmin):
@@ -21,19 +23,20 @@ class PedidoAdmin(admin.ModelAdmin):
         'total',
     )
 
-    def total(self, obj):
-        return obj.get_total()
-
-    total.short_description = 'Total'
-
     fieldsets = (
         ('Dados do Cliente', {'fields': ('nome_cliente', 'telefone', 'endereco')}),
         ('Dados do Pedido', {'fields': ('acai_pedido', 'acrescimo_pedido', 'pagamento', 'observacoes', 'valor_pagar')}),
     )
 
     readonly_fields = ('valor_pagar',)
-    search_fields = ('id', 'nome_cliente')
-    raw_id_fields = ('acai_pedido', 'acrescimo_pedido')
+    search_fields = ('id', 'nome_cliente',)
+    autocomplete_fields = ('acai_pedido', 'acrescimo_pedido')
+
+
+    def total(self, obj):
+        return obj.get_total()
+
+    total.short_description = 'Total'
 
 
 admin.site.register(CadastroAcai, CadastroAcaiAdmin)
